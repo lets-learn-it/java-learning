b36762bdbe7437ea60c3b0969cf8142bcb28523a
Parikshit Patil <patil8698parikshit@gmail.com>
2022-10-02 18:45:21 +0530

    hello world

---
+++ 00-hello-world/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+  /* 
+   * This is multiline comment
+   * @author: PSKP_95
+   * 
+   * public: access modifier, which sets visibility
+   * static: main method to be allowed without instantiating class
+   * void: main method not returning anything
+   * args: cmd line args
+   */
+  public static void main(String[] args) {
+    // This is single line comment
+    System.out.println("Hello, World!");
+  }
+}
---
+++ 00-hello-world/README.md
@@ -0,0 +1,17 @@
+# Hello World
+
+## Running Program
+
+### Compiling
+
+```bash
+javac Main.java
+```
+
+Above step will create class file named `Main.class`.
+
+### Execute
+
+```bash
+java Main
+```
